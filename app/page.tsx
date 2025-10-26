35fcebc8189974f541e0965592f374ca40468b3a
maxubrq <hungtp.play@gmail.com>
2025-10-26 20:11:33 +0700

    feat: add suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import { getSortedPostsData } from '@/lib/posts'
 import { PostsContainer } from '@/components/PostsContainer'
+import { Suspense } from 'react'
 
 export default function Home() {
   const allPosts = getSortedPostsData()
 
-  return <PostsContainer allPosts={allPosts} />
+  return <Suspense fallback={<div>Loading...</div>}>
+    <PostsContainer allPosts={allPosts} />
+  </Suspense>
 }
